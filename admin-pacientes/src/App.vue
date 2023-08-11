7a60d6a0c93ed449f4e9b8fca52d795ac7b15235
Daniel Sánchez Casas <139398686+DanielSanchezCasas@users.noreply.github.com>
2023-08-11 16:36:11 -0500

    Juan Pablo de la Torre Vue.js project #2 creation

---
+++ admin-pacientes/src/App.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { ref, reactive, onMounted, watch } from "vue";
+import { uid } from "uid";
+import Header from './components/Header.vue';
+import Formulario from './components/Formulario.vue';
+import Paciente from './components/Paciente.vue'
+
+const pacientes = ref([]);
+
+const paciente = reactive({
+  id: null,
+  nombre: '',
+  propietario: '',
+  email: '',
+  fechaAlta: '',
+  sintomas: '',
+});
+
+watch(pacientes, () => {
+  guardarLocalStorage();
+},{
+  deep: true,
+})
+
+onMounted(() => {
+  const pacientesStorage = localStorage.getItem('pacientes');
+  if(pacientesStorage){
+    pacientes.value = JSON.parse(pacientesStorage);
+  }
+})
+
+const guardarLocalStorage = () => {
+  localStorage.setItem('pacientes', JSON.stringify(pacientes.value));
+}
+
+const guardarPaciente = (() => {
+  if(paciente.id){
+    const { id } = paciente;
+    const i = pacientes.value.findIndex(paciente=> paciente.id === id);
+    pacientes.value[i] = {...paciente};
+  }else{
+    pacientes.value.push({ ...paciente, id:uid() });
+  }
+  
+  Object.assign(paciente, {
+    id: null,
+    nombre: '',
+    propietario: '',
+    email: '',
+    fechaAlta: '',
+    sintomas: '',
+  })
+})
+
+const actualizarPaciente = ((id) =>{
+  const pacienteEdit = pacientes.value.filter(paciente => paciente.id === id)[0];
+  Object.assign(paciente, pacienteEdit);
+})
+
+const eliminarPaciente = ((id) =>{
+  pacientes.value = pacientes.value.filter(paciente => paciente.id !== id);
+})
+
+</script>
+
+<template>
+  <div class="container mx-auto mt-20">
+    <Header />
+    <div class="mt-12 md:flex">
+      <Formulario v-model:nombre="paciente.nombre" v-model:propietario="paciente.propietario"
+        v-model:email="paciente.email" v-model:fechaAlta="paciente.fechaAlta" v-model:sintomas="paciente.sintomas"
+        @guardar-paciente="guardarPaciente" :id="paciente.id"/>
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">Administre sus pacientes</h3>
+        <div v-if="pacientes.length > 0">
+          <p class="text-lg mt-5 text-center mb-10">
+            Informacion de <span class="text-indigo-600 font-bold">Pacientes</span>
+          </p>
+          <Paciente v-for="paciente in pacientes" :paciente="paciente" @actualizar-paciente="actualizarPaciente" @eliminar-paciente="eliminarPaciente"/>
+        </div>
+        <p v-else class="mt-10 text-2xl text-center">No hay pacientes</p>
+      </div>
+    </div>
+  </div>
+</template>
+
